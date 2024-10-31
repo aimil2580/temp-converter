5a04fe4e6bb244334e06e64bbbb6fbc3196b8836
aimilPCLinux <aimil2580@proton.me>
2024-10-31 15:05:42 +0200

    needs to be cheacked again frfr

--- dates/main.js
+++ dates/main.js
@@ -2,14 +2,37 @@ const selectWays = document.querySelector(".selectWays")
 const oneWay = document.querySelector(".oneWay")
 const returnWay = document.querySelector(".returnWay")
 const bookBtn = document.querySelector(".book")
+
 const date = new Date();
+const newDate = (`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`)
 
-
-
+//idk what is the select listener so i used click (wrong btw)
 selectWays.addEventListener("click", () => {
     if (selectWays.value === "oneWay") {
+        returnWay.setAttribute("disabled", "")
+    } else {
+        returnWay.removeAttribute("disabled")
+    }
+})
+
+oneWay.addEventListener("input", () => {
+    if (oneWay.value.toString() < newDate.toString()) {
         bookBtn.setAttribute("disabled", "")
-    } else{
+        bookBtn.textContent = "Please book a valid date";
+        returnWay.setAttribute("disabled", "")
+    } else {
         bookBtn.removeAttribute("disabled")
+        bookBtn.textContent = "Book!";
+        returnWay.removeAttribute("disabled")
+    }
+})
+
+returnWay.addEventListener("input", () => {
+    if (returnWay.value.toString() < oneWay.value.toString() || returnWay.value.toString() < newDate.toString() || oneWay.value.toString() < newDate.toString()){
+        bookBtn.setAttribute("disabled", "")
+        bookBtn.textContent = "Please book a valid date for return";
+    } else {
+        bookBtn.removeAttribute("disabled")
+        bookBtn.textContent = "Book!";        
     }
 })
